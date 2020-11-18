6e7d4975f2650245e27e469279f42fe1d7ec47ee
Baudoin HAAS <bahaas@e2r11p19.42.fr>
2020-11-18 15:17:15 +0100

    Fix: Correct norm on memccpy

--- srcs/ft_memccpy.c
+++ srcs/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: bahaas <bahaas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/16 22:52:10 by bahaas            #+#    #+#             */
-/*   Updated: 2020/11/18 14:41:11 by bahaas           ###   ########.fr       */
+/*   Updated: 2020/11/18 15:16:54 by bahaas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*tmp_dest;
 	unsigned char	*tmp_src;
 
